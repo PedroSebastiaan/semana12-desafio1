2da05add800ede91866803d687c4281bbb1bf09f
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-28 15:08:51 -0400

    add: dates validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,12 @@ class Book < ApplicationRecord
     validates :author, presence: true
     validates :status, presence: true
     enum status: %w[House Outside]
+
+    validate :valid_dates
+
+    def valid_dates
+        if lending_date > return_date 
+        self.errors.add :return_date, 'must be after than lending date.'
+        end
+    end
 end
