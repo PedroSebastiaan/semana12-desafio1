edbf9ad8bba40c222b121f97b53675c45158fdc8
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-28 16:07:45 -0400

    add: blankspace in datetime

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,8 +7,10 @@ class Book < ApplicationRecord
     validate :valid_dates
 
     def valid_dates
-        if lending_date > return_date 
-        self.errors.add :return_date, 'must be after than lending date.'
+        if lending_date != nil || return_date != nil 
+            if lending_date > return_date 
+                self.errors.add :return_date, 'must be after than lending date.'
+            end
         end
     end
 end
